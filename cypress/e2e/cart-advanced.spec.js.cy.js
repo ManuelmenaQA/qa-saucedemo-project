4830abe6020c7a876880b89af2ab49a3fe75e48f
Manuel Mena <manolito_91@hotmail.com>
2026-04-01 20:32:31 +0200

    Cart Advanced Tests

---
+++ cypress/e2e/cart-advanced.spec.js.cy.js
@@ -0,0 +1,36 @@
+describe('Cart Advanced Tests - SauceDemo', () => {
+
+  beforeEach(() => {
+    cy.visit('www.saucedemo.com')
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="login-button"]').click()
+  })
+
+  it('TC-11: Añadir múltiples productos al carrito', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+  })
+
+  it('TC-12: Validar precio total', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="checkout"]').click()
+    cy.get('[data-test="firstName"]').type("Juan")
+    cy.get('[data-test="lastName"]').type("Perez")
+    cy.get('[data-test="postalCode"]').type("28001")
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="total-label"]').should('contain', 'Total: $43.18')
+  })
+
+   it('TC-13: Eliminar producto del carrito', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+    cy.get('[data-test="remove-sauce-labs-bike-light"]').click()
+  })
+
+})
